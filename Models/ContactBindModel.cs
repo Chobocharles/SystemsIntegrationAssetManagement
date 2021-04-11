a74624764149bcd7838bfeeb299bf719e9f3bc31
Jonathan Trombley <jonathantrombley@outlook.com>
2021-04-11 02:14:27 -0400

    added model binding for search support

---
+++ Models/ContactBindModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asset_Management.Models
+{
+    public class ContactBindModel
+    {
+        [Display(Name = "User ID")]
+        [Required, MaxLength(255)]
+        [MinLength(1, ErrorMessage = "Please enter at least 1 character")]
+        public string UserID { get; set; }        
+    }
+}
---
+++ Models/CreateContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asset_Management.Models.SQL;
+
+namespace Asset_Management.Models
+{
+    public class CreateContact
+    {
+        public Contact Contact { get; set; }
+        public ContactBindModel Search { get; set; }
+    }
+}
