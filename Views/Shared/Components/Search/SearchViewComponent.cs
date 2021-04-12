16803055e54dc719d365a34f6de3f07a5e954b8c
Jonathan Trombley <jonathantrombley@outlook.com>
2021-04-11 23:22:09 -0400

    updated layout and moved search to separeate compnent for strong model utilization

---
+++ Views/Shared/Components/Search/SearchViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Asset_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asset_Management.Views.Shared.Components.Search
+{
+    public class SearchViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new SearchBindModel();
+            return View(model);
+        }
+    }
+}
